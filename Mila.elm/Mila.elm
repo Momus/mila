bb1f35c0616468e7143107277ccff60b29af7704
Dmitri G. Brengauz <dmitri@momus.net>
2016-05-02 15:08:06 -0600

    ¡MILA! pròximament

--- Mila.elm/Mila.elm
+++ Mila.elm/Mila.elm
@@ -1,11 +1,30 @@
 
 module Mila (..) where
 
-import String exposing (..)
 import Html exposing (..)
+import Html.Attributes exposing (..)
+
+import String
+
+
+title message times =
+  message
+    |> String.toUpper
+    |> String.repeat times
+    |> text
+
+pageHeader =
+  h1 [ ] [ title   "¡Mila! " 3 ]
+
+
+pageFooter =
+  h3 [ ]
+    [ text "pròximament"
+    ]
+
+view =
+  div [ id "container" ]
+      [ pageHeader, pageFooter ]
 
 main =
-  "Mila! "
-    |> String.toUpper
-    |> String.repeat 3
-    |> text
+  view
